f151366d2b3edf21a1f55caebfa371b17887cfd7
Nathan Parker <natecoleparker@gmail.com>
2024-08-09 23:15:40 -0400

    💪 Proper Page Navigation
    - Can navigate between pages with correct animations
    - Nav and other elements persist when needed
    
    - Still need to work on page titles and same page animations.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
-
 import vtbot from "astro-vtbot";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon({
     iconDir: "public/assets/icons"
-  }), vtbot()],
+  }), vtbot(), react()],
   build: {
     assets: 'app'
   }
