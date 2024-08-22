c73e7f275775fe01535e9c97324b2d7222a9bad8
Nathan Parker <natecoleparker@gmail.com>
2024-08-21 23:32:27 -0400

    🔗 Video Embed
    - Can see video attrs

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,1 +1,34 @@
 /* This file is your content script. */
+
+function startup() {
+	console.log('Starting up');
+	var video = document.querySelector('.html5-main-video') as HTMLVideoElement;
+
+	setInterval(() => {
+		console.clear();
+		console.log('Video found', video);
+		console.log('Autoplay?', video.autoplay);
+		console.log('Muted?', video.muted);
+		console.log('Volume?', video.volume);
+		console.log('Current time?', video.currentTime);
+		console.log('Duration?', video.duration);
+		console.log('Paused?', video.paused);
+		console.log('Ended?', video.ended);
+		console.log('Seeking?', video.seeking);
+		console.log('Buffered?', video.buffered);
+		console.log('Loop?', video.loop);
+		console.log('Playback rate?', video.playbackRate);
+	}, 1000);
+}
+
+document.addEventListener('yt-navigate-finish', function() {
+	let url = new URL(document.URL);
+
+	if (url.hostname == 'www.youtube.com') {
+		if (url.pathname == '/watch') {
+			startup();
+		}
+	}
+});
+
+console.log('Content script loaded');
